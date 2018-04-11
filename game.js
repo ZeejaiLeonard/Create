64c4fa2efd2b559c1d1243988cff71c2f6570080
Zeejai Leonard <793313@seq.org>
2018-04-10 17:07:15 -0700

    concept developing, organizing

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 window.onload = init;
-var game, linebreak;
+var game;
+var rooms = ["hall", "dungeon", "waterfall"];
+
 
 function init(){
   game = new Game();
@@ -7,19 +9,13 @@ function init(){
   animate();
 }
 
-function pageScroll() {
-  window.scrollBy(0, 1);
-  scrolldelay = setTimeout(pageScroll, 10);
-}
-
-function displayOutput(output){
-  document.getElementById("story").appendChild(document.createTextNode(output));
-  document.getElementById("story").appendChild(document.createElement("br"));
-}
-
 function checkInput(){
   var input = document.getElementById("bar").value;
-  displayOutput(input);
+  if (input === "inventory") {
+    displayOutput(player.inventory[0]);
+  } else {
+    displayOutput(input);
+  }
 }
 
 function animate(){
@@ -30,29 +26,50 @@ function animate(){
 function Game(){
   this.map = [];
   this.tileSize = 10;
+  //this.playerPosition = new JSVector(0, 0);
 
   this.init = function(){
-    for(var i = 0; i < 10; i++){
+    for (var i = 0; i < 10; i++) {
       this.map.push([]);
-      for(var j = 0; j < 10; j++){
-        this.map[i].push(j);
+      for (var j = 0; j < 10; j++) {
+        this.map[i].push(new Room(rooms[generateRandomInt(rooms.length)]));
       }
     }
+    //organize
   }
+}
 
-  this.update = function(){
+function Room(name){
+  this.name = name;
+  this.contents = [];
+  this.scene = "";
+}
 
-  }
-
+function Object(){
+  this.health = 0;
 }
 
 function Player(){
-  this.location = "null";
-  this.position = 0;
+  this.position = new JSVector(0, 0);
+  this.inventory = [];
 
-  this.update = function(){
-
+  this.update = function(locationVector){
+    this.position = locationVector;
   }
+}
+
+function generateRandomInt(max){
+  return Math.floor(Math.random() * max);
+}
+
+function pageScroll() {
+  window.scrollBy(0, 1);
+  scrolldelay = setTimeout(pageScroll, 10);
+}
+
+function displayOutput(output){
+  document.getElementById("story").appendChild(document.createTextNode(output));
+  document.getElementById("story").appendChild(document.createElement("br"));
 }
 
 window.addEventListener("keypress", function(event){
